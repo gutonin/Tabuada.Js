9b131387134517548fac817c564fa3737af8dd75
Gustavo Tonin <36697606+gutonin@users.noreply.github.com>
2019-04-15 16:19:15 -0300

    Add files via upload

---
+++ tabuada.js
@@ -0,0 +1,11 @@
+/**
+ * Tabuada em Js usando For
+ * @author Gustavo Tonin
+ */
+
+for (var i = 1; i < 11; i++){
+    document.write("<p>Tabuada do " + i + "</p>");
+    for (var j = 1; j < 11; j++){
+        document.write(i + " x " + j + " = " + (i * j) + "</br>")
+    }
+}
